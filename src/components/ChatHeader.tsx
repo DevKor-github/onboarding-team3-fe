a3d1fa329855fa13a93fe1603555af02d3e63234
chsein <chltpdls4087@gmail.com>
2024-08-26 17:29:54 +0900

    feat:deletechat

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,13 +1,33 @@
 // src/components/ChatHeader.tsx
+import axios from 'axios';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface ChatHeaderProps {
   username: string;
   profileImageUrl: string;
+  roomNumber: number
 }
 
-const ChatHeader: React.FC<ChatHeaderProps> = ({ username, profileImageUrl }) => {
+
+const ChatHeader: React.FC<ChatHeaderProps> = ({ username, profileImageUrl, roomNumber }) => {
   const defaultImageUrl = "/src/assets/profile.png";
+  const navigate = useNavigate();
+
+  const deleteChatRoom = async (roomNumber: number) => {
+    try {
+      const token = localStorage.getItem('authToken');
+        
+      // Send a DELETE request
+      const response = await axios.delete(`http://ec2-43-203-30-181.ap-northeast-2.compute.amazonaws.com:8080/api/chat/${roomNumber}`);
+        navigate('/chat/list');
+      
+      console.log('DeleteChatRoom API response:', response);
+  
+    } catch (error) {
+      console.error('Error deleting chat room:', error);
+    }
+  };
 
   return (
     <div className="w-[393px] h-[62px] pl-[24px] py-[15px] bg-white flex justify-between items-center">
@@ -25,6 +45,12 @@ const ChatHeader: React.FC<ChatHeaderProps> = ({ username, profileImageUrl }) =>
           {username}
         </div>
       </div>
+      <button
+        onClick={() => deleteChatRoom(roomNumber)} // Trigger chat room creation on button click
+        className="text-black px-2 py-1 text-sm line"
+      >
+        Delete
+      </button>
     </div>
   );
 };
